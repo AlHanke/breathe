1277d73ad1fdfb6047e9a59fc15a0206a895d18c
Alexander Hanke <alexander.hanke@ntnu.no>
2024-06-28 14:29:43 +0200

    Done

--- breathe/parser/compoundsuper.py
+++ breathe/parser/compoundsuper.py
@@ -1361,8 +1361,6 @@ class memberdefType(GeneratedsSuper):
             obj_ = paramType.factory()
             obj_.build(child_)
             self.param.append(obj_)
-            with open('out2.txt', 'a') as f:
-                print(obj_.defname, file=f)
         elif child_.nodeType == Node.ELEMENT_NODE and \
             nodeName_ == 'enumvalue':
             obj_ = enumvalueType.factory()
