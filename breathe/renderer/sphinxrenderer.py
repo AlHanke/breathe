b023bfcea264321bfdd667aa78071b35324564fb
Alexander Hanke <alexander.hanke@ntnu.no>
2024-06-28 13:53:12 +0200

    test

--- breathe/renderer/sphinxrenderer.py
+++ breathe/renderer/sphinxrenderer.py
@@ -2002,12 +2002,12 @@ class SphinxRenderer:
                 elements.append(name)
                 with open('out.txt', 'a') as f:
                     if len(node.get_param())>0:
-                        print(node.get_param()[0].type_.content_[0].value, file=f)
+                        print(node.get_param()[0].type_.content_[0].value.content_, file=f)
                         print(node.get_param()[0].declname, file=f)
-                        print(node.get_param()[0].defname, file=f)
-                        print(node.get_param()[0].array, file=f)
-                        print(node.get_param()[0].defval, file=f)
-                        print(node.get_param()[0].briefdescription, file=f)
+                        # print(node.get_param()[0].defname, file=f)
+                        # print(node.get_param()[0].array, file=f)
+                        # print(node.get_param()[0].defval, file=f)
+                        # print(node.get_param()[0].briefdescription, file=f)
                 elements.append(node.get_argsstring())
                 declaration = " ".join(elements)
             nodes = self.handle_declaration(node, declaration)
